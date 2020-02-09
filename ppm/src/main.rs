1a593fff25ae90ba6ad0bfe34814e30d460e4ff4
Souissi Mohamed <mohaxoy98@gmail.com>
2020-02-09 23:09:52 +0100

    test parsing ppm files and transforming ppm files into an image and saving image into file

--- ppm/src/main.rs
+++ ppm/src/main.rs
@@ -16,7 +16,8 @@ struct Image {
     pixels: Vec<Pixel>,
     heigth: usize,
     width: usize,
-    rgbType: String
+    rgbType: String,
+    maxVal: u8
 }
 
 #[link(name = "c")]
@@ -83,24 +84,49 @@ fn new_with_file(filename: &Path) -> Image {
     let mut finalPixels : Vec<Pixel>  = vec![];
     let mut finalHeight : usize = 0;
     let mut finalWidth : usize = 0;
+    let mut finalmaxVal : u8 = 0;
     let mut typeRgb = String::from("");
     for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); 
+        let line = line.unwrap();
             let parsedLine:Vec<&str>= line.split(" ").collect();
-            if parsedLine.len() == 2 {
-                finalHeight = parsedLine[0].parse::<usize>().unwrap();
-                finalWidth =  parsedLine[1].parse::<usize>().unwrap();
-            } else if parsedLine.len() == 3 {
-                let linePixel:Vec<&str>= line.split(" ").collect();
-      
-                let finalPixel = Pixel {
-                    rouge : linePixel[0].parse::<u8>().unwrap(),
-                    bleu:  linePixel[1].parse::<u8>().unwrap(),
-                    vert: linePixel[2].parse::<u8>().unwrap()
-                };
-                finalPixels.push(finalPixel);
-            } else if parsedLine.len() == 1 {
-                typeRgb = parsedLine[0].to_string();
+            if parsedLine[0].chars().next().unwrap() != '#' {
+                if parsedLine.len() == 2 {
+                    finalHeight = parsedLine[0].parse::<usize>().unwrap();
+                    finalWidth =  parsedLine[1].parse::<usize>().unwrap();
+                } else if parsedLine.len() >= 3 {
+                    // println!("{} {}  {}\n", parsedLine[0], parsedLine[1], parsedLine[2]);
+                    let mut index = 0;
+                    let mut rougef : u8 = 0;
+                    let mut bleuf : u8 = 0;
+                    let mut vertf : u8 = 0;
+                    for i in 0..parsedLine.len() {
+                        if parsedLine[i].is_empty() == false && parsedLine[i].find(|c: char| (c < '0') || (c > '9')) == None {
+                            println!("{} {}\n", parsedLine[i], i);
+                            match index {
+                                // Match a single value
+                                0 => rougef = parsedLine[i].parse::<u8>().unwrap(),
+                                1 => bleuf = parsedLine[i].parse::<u8>().unwrap(),
+                                2 => vertf = parsedLine[i].parse::<u8>().unwrap(),
+                                _ => println!("")
+                            }
+                            index = index + 1;
+                        }
+                    }
+                    let finalPixel = Pixel {
+                        rouge : rougef,
+                        bleu:  bleuf,
+                        vert: vertf
+                    };
+                    // println!("Rouge = {}\nBleu = {}\nVert = {}", finalPixel.rouge, finalPixel.bleu, finalPixel.vert);
+                    finalPixels.push(finalPixel);
+                } else if parsedLine.len() == 1 {
+                    if parsedLine[0].find(|c: char| (c < '0') || (c > '9')) == None {
+                        finalmaxVal = parsedLine[0].parse::<u8>().unwrap();
+                    }
+                    else {
+                        typeRgb = parsedLine[0].to_string();
+                    }
+                }
             }
             // still lines wich contains # or the of RGB color
         }
@@ -108,7 +134,8 @@ fn new_with_file(filename: &Path) -> Image {
         pixels : finalPixels,
         heigth: finalHeight,
         width: finalWidth,
-        rgbType: typeRgb
+        rgbType: typeRgb,
+        maxVal: finalmaxVal
     }
 }
 
@@ -117,12 +144,16 @@ impl Image {
   fn save(&self, filename: &Path) {
       let mut file = File::create(filename).unwrap();
       file.write_all(self.rgbType.as_bytes()).unwrap();
+      file.write_all(b"\n");
       let secondLine = String::from(self.heigth.to_string()+" "+&self.width.to_string());
       file.write_all(secondLine.as_bytes()).unwrap();
-      
+      file.write_all(b"\n");
+      file.write_all(self.maxVal.to_string().as_bytes()).unwrap();
+      file.write_all(b"\n");
       for i in 0..self.pixels.len() {
         let pixelLine = String::from(self.pixels[i].rouge.to_string()+" "+&self.pixels[i].bleu.to_string()+" "+&self.pixels[i].vert.to_string());
         file.write_all(pixelLine.as_bytes()).unwrap();
+        file.write_all(b"\n");
       }
       //close the file
       drop(file);
@@ -131,5 +162,8 @@ impl Image {
 
 fn main() {
     let x = unsafe { sin(10) };
+    let image = new_with_file(Path::new("/home/moxa/Bureau/ESGI/Programmation système et réseau/ppm/ppm/src/test.txt"));
+    let output = Image { heigth: image.heigth, width: image.width, pixels: image.pixels, rgbType: image.rgbType, maxVal: image.maxVal};
+    output.save(Path::new("/home/moxa/Bureau/ESGI/Programmation système et réseau/ppm/ppm/src/res.txt"));
     println!("double de 100 {}", x);
 }
